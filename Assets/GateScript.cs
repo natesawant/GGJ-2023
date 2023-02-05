d330341b8b38a5f8b2f85339c0a2f357fbdbf264
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2023-02-05 11:27:53 -0500

    Added noises to gates and buttons

--- Assets/GateScript.cs
+++ Assets/GateScript.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class GateScript : ActivatableInterface
 {
     public GameObject opened, closed;
+    public AudioClip openNoise, closeNoise;
+    AudioSource audioSrc;
+
+    private void Start()
+    {
+        audioSrc= GetComponent<AudioSource>();
+    }
     public override void Activate()
     {
         opened.SetActive(true);
         closed.SetActive(false);
+        audioSrc.clip = openNoise;
+        audioSrc.Play();
     }
 
     public override void Deactivate()
     {
         opened.SetActive(false);
         closed.SetActive(true);
+        audioSrc.clip = closeNoise;
+        audioSrc.Play();
     }
 }
